578302e36bde72f0b12d93c43d93cf8d8c2f7bf0
Mohammed Abdul Abi <mohammedabdulabi@gmail.com>
2026-06-09 22:26:58 +0200

    fix: resolve CORS by using direct API URLs

--- src/services/sculptures.ts
+++ src/services/sculptures.ts
@@ -3,7 +3,7 @@
 import type { SculptureType } from "../api/sculptureTypes";
 
 const SCULPTURES_URL =
-  "api/dataset/afae0c45-1c59-44b5-a433-a6c61da2cb2e/resource/d5fc6de8-7e27-434b-9927-0bf531352b17/download/skulpturer-i-stavanger.json";
+  "https://opencom.no/dataset/afae0c45-1c59-44b5-a433-a6c61da2cb2e/resource/d5fc6de8-7e27-434b-9927-0bf531352b17/download/skulpturer-i-stavanger.json";
 
 export async function fetchSculptures(): Promise<SculptureType[]> {
   const res = await fetch(SCULPTURES_URL);
--- src/services/solvbergetApi.ts
+++ src/services/solvbergetApi.ts
@@ -1,7 +1,7 @@
 import type { SolvbergetRaw } from "../api/solvbergetTypes";
 
 const API_URL =
-  "/api/dataset/a80639f3-2f64-4f26-9f1b-7da12f04970c/resource/e7419316-b181-44fa-9ab1-6151e80fafdc/download/folkevandring-solvberget.json";
+  "https://opencom.no/dataset/a80639f3-2f64-4f26-9f1b-7da12f04970c/resource/e7419316-b181-44fa-9ab1-6151e80fafdc/download/folkevandring-solvberget.json";
 
 export async function fetchSolvbergetData(): Promise<SolvbergetRaw[]> {
   if (!API_URL) {
